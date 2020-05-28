2d8521089911b7ee6fd00091105fea63b4b66f9f
KevinVaha <59019900+KevinVaha@users.noreply.github.com>
2020-05-28 21:44:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+$(function () {});
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+    //Toggle nav
+    nav.classList.toggle('nav-active');
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+  });
+};
+navSlide();
+
+$('a').click(function () {
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    1000
+  );
+  return false;
+});
